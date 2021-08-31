ea937687ae1189b9215ca4bc260af8e6389f5c44
Avat <avatolzhobaeva@gmail.com>
2021-08-31 18:44:21 +0600

    Testing changes

--- categories/views.py
+++ categories/views.py
@@ -17,6 +17,7 @@ from rest_framework import status
 
 class CategoriesListVIew(ListAPIView):
     serializer_class = CategorySerializer
+    print('sdaf')
     def get_queryset(self):
         return Category.objects.all()
 
